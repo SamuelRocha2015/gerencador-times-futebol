fd2394c5b69f954df846be7f0b61743036a2e139
Samuel Rocha <samuelrodriguessr@hotmail.com>
2018-12-16 21:33:08 -0300

    refactor data insert

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,15 @@
 insert into TIMES
 values(10001,'branco vermelho e preto', 'vermelho e branco', now(), 'santa cruz');
+insert into TIMES
 values(10002,'branco e vermelhor', 'branco', now(), 'nautico');
 
 insert into JOGADOR
 values(10001, null, TRUE, 5, 'roberto durval', 10000.00, 10001);
+insert into JOGADOR
 values(10002, null, FALSE, 4, 'paulo roberto', 9000.00, 10001);
+insert into JOGADOR
 values(10003, null, FALSE, 3, 'andrei santos', 8000.00, 10001);
+insert into JOGADOR
 values(10004, null, TRUE, 5, 'joao paulo', 10000.00, 10002);
+insert into JOGADOR
 values(10005, null, FALSE, 4, 'andre macedo', 9000.00, 10002);
