7da37d729064d3a9a09887b94ef1ed9a8a792210
Samuel Rocha <samuelrodriguessr@hotmail.com>
2018-12-16 21:20:32 -0300

    data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,10 @@
 insert into TIMES
 values(10001,'branco vermelho e preto', 'vermelho e branco', now(), 'santa cruz');
-
+values(10002,'branco e vermelhor', 'branco', now(), 'nautico');
 
 insert into JOGADOR
-values(10001, null, FALSE, 5, 'roberto durval', 10000.00, 10001)
+values(10001, null, TRUE, 5, 'roberto durval', 10000.00, 10001);
+values(10002, null, FALSE, 4, 'paulo roberto', 9000.00, 10001);
+values(10003, null, FALSE, 3, 'andrei santos', 8000.00, 10001);
+values(10004, null, TRUE, 5, 'joao paulo', 10000.00, 10002);
+values(10005, null, FALSE, 4, 'andre macedo', 9000.00, 10002);
